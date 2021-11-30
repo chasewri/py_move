ab8ce91e5252c8987ad9d3a2331068938ed0a75d
Chase Wright <chasewri@gmail.com>
2021-11-30 16:31:41 -0700

    removed tabs

--- move.py
+++ move.py
@@ -7,16 +7,16 @@ import random
 i = 0
 move = 10
 sleep_time = 20
-run_option = int(input('Enter 1 for notabs or 2 for tabs: '))
+# run_option = int(input('Enter 1 for notabs or 2 for tabs: '))
 
 while True:
     if i % 2 == 0:
         move = random.randint(5, 25)
         pyautogui.moveRel(0, move)
-        if run_option != 1 and move > 15:
-            for _ in range(2):
-                with pyautogui.hold('command'):
-                    pyautogui.press(['tab'])
+        # if run_option != 1 and move > 15:
+        # for _ in range(2):
+        #     with pyautogui.hold('command'):
+        #         pyautogui.press(['tab'])
     else:
         pyautogui.moveRel(0, -move)
 
