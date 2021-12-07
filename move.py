918f144902004af73e08265ad198b0a559ad700f
Chase Wright <chasewri@gmail.com>
2021-12-07 16:59:07 -0700

    adds updates

--- move.py
+++ move.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
 
 import pyautogui
 import time
@@ -7,20 +7,17 @@ import random
 i = 0
 move = 10
 sleep_time = 20
-# run_option = int(input('Enter 1 for notabs or 2 for tabs: '))
+print('Thanks for stopping by')
 
 while True:
     if i % 2 == 0:
         move = random.randint(5, 25)
         pyautogui.moveRel(0, move)
-        # if run_option != 1 and move > 15:
-        # for _ in range(2):
-        #     with pyautogui.hold('command'):
-        #         pyautogui.press(['tab'])
+
     else:
         pyautogui.moveRel(0, -move)
 
     i += 1
 
-    sleep_time = random.randint(15, 35)
+    sleep_time = random.randint(5, 35)
     time.sleep(sleep_time)
